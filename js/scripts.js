2c892e91a08dcf1445bb00d85002e5f57904f636
Musab36 <musabhd@gmail.com>
2017-05-08 17:50:28 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
-	 $("#sub").submit(function(event) {
-	 	alert("Thanks for subscribing!");
-
+	 $(".user").submit(function(event) {
+	 	document.write("SignUp Successful");
 	 	event.preventDefualt();
 	 });
 
 	 $("#hmo").click(function() {
 	 	 $(".all").hide(5000);
+           $("#nws").hide(500);
+           $(".pht").hide(500);
 	 });
 
      $("#mrm").click(function() {
      	 $(".all").show(6000);
+           $("#nws").hide(500);
      });
-     
+
+     $("#new").click(function() {
+     	 $("#nws").show(5000);
+           $(".all").hide("slow");
+           $(".pht").hide(5000);
+     });
+
+     $("#logi").submit(function(event) {
+          event.preventDefualt();
+               
+          });
+
+     $("#ft").click(function() {
+          $(".pht").show(5000);
+          $(".all").hide(5000);
+          $("#nws").hide(5000);
+     });
      });
